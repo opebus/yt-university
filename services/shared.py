f56f2525b669d05e9854d87464208130e8c67c32
Wei Chun <tanweichun@outlook.com>
2024-04-09 09:32:29 +0800

    scaffold: saving to supabase

--- services/shared.py
+++ services/shared.py
@@ -11,9 +11,20 @@ def process(video_url):
     id = uuid.uuid4()
 
     f = Function.lookup("yt-university", "Downloader.run")
-    call = f.spawn(video_url, f"{id}/")
-    audio_path = functions.gather(call)
+    call = f.spawn(video_url, id)
+    results = functions.gather(call)
+    audio_path, thumbnail_path, metadata = results[0]
 
-    call = transcribe.spawn(audio_path[0], f"{id}/transcription.json")
-    res = functions.gather(call)
-    return res
+    # with open(audio_path, 'rb') as f:
+    #     supabase.storage.from_("audio").upload(file=f, path=audio_path.split("/")[-1])
+
+    # with open(thumbnail_path, 'rb') as f:
+    #     supabase.storage.from_("thumbnails").upload(file=f, path=thumbnail_path.split("/")[-1])
+
+    # supabase.table("videos").upsert(metadata, ["id"])
+
+    call = transcribe.spawn(audio_path)
+    results = functions.gather(call)
+    transcription = results[0]
+
+    # supabase.table("videos").upsert(transcription, ["id"])
